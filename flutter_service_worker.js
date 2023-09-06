5ca0905f157f6aa0532d4bf05d067689e084f1b6
Abdelkader SEBIHI <49540075+aymensbh@users.noreply.github.com>
2023-09-06 13:57:56 +0100

    update added filter by date

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,11 +4,11 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "e70b850d429bd24383462393cf63316c",
-"index.html": "804504ba23f88889466fe048760973a3",
-"/": "804504ba23f88889466fe048760973a3",
-"main.dart.js": "98fbca4f92465b16f05f830fa2985f8c",
+"index.html": "3acf542298c87330e33268500ddf4b32",
+"/": "3acf542298c87330e33268500ddf4b32",
+"main.dart.js": "cc8484c802dae1d6f1038d4541f39654",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"favicon.png": "4aa85ef1a10eb03705646b8e27696c1b",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
