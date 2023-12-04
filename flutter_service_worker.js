c5c70b0519a2e061639fd3ffd8049f602aef0979
Abdelkader SEBIHI <49540075+aymensbh@users.noreply.github.com>
2023-12-04 16:58:58 +0100

    ui updates

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"version.json": "2b771173e18da62135b3a0dd57b89c48",
-"index.html": "5b88cd1245c694f08d7ed0315a50e7d6",
-"/": "5b88cd1245c694f08d7ed0315a50e7d6",
-"main.dart.js": "8276b52a286b7f1bffcce2e90c633485",
+"index.html": "3c49ad8d3d872ad04a4f9f7d12b4bb93",
+"/": "3c49ad8d3d872ad04a4f9f7d12b4bb93",
+"main.dart.js": "75676a4f913dcf1a5c123d8fd49861af",
 "flutter.js": "6fef97aeca90b426343ba6c5c9dc5d4a",
 "favicon.png": "831256f7f299e307b25a16937fbd675e",
 "icons/favicon.ico": "f58885b6fdf94d414e016605181dca2f",
